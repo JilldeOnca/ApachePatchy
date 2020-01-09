df8780e687966c8a79bfe4b802a94148773886d3
vectorXX <kgs23@njit.edu>
2020-01-09 13:37:01 -0500

    Replaces DisableSymLinks.sh
    DisableSymLinks.sh (deleted) is now this file UnfollowSymLinks.sh (uploaded).
    Can't rename a file via the github GUI!

---
+++ UnfollowSymLinks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash  
+source BackupFile.sh   
+#APACHECONFIG='/etc/apache2/apache2.conf'
+APACHECONFIG='apache2.conf'      
+
+UnfollowSymLinks(){
+    count=$(cat ${APACHECONFIG} | grep  -B 1 FollowSymLinks | grep -c "<Directory />")
+    #echo "count = ${count}"
+
+    if [ ${count} = 1 ]; then 
+        echo "Best practices violation using 'Option FollowSymLinks':"
+        echo "The root directory ('/') should not include the 'Option FollowSymLinks'."
+        echo "Would you like to remove this property? (y/n) "
+        read -r answer
+
+        if [ ${answer} == 'y' ]; then
+            echo "Backing up apache2.config file....."
+            # backup function found in BackupFile.sh
+            backup apache2.conf
+            echo "...."
+            sleep .25
+            echo "Updating apache2.config file....."
+            sleep .25
+            echo "Disabling symbolic links for root directory......"
+            sleep .25
+            #here code to replace the line containing option indexes, with the line not containing option indexes (cut indexes)
+            sed -i 's/ FollowSymLinks//' ${APACHECONFIG}
+            #sed -i 's/+FollowSymLinks/-FollowSymLinks/' apache2.conf
+            echo "........."
+            sleep .25
+            echo "apache2.config file successfully updated!"
+            
+        elif [ ${answer} == 'n' ]; then
+            echo "Best practice not accepted by admin. Option FollowSymLinks in root directory will remain unchanged."
+
+        else 
+            echo "Please enter y for Yes or n for No" 
+        fi;
+    else
+        echo "The root directory is not configured to follow symbolic links. Your server is already in compliance, no changes made."
+    fi;
+}
