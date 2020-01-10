3f41d5d1cfbfa620f34578d7e611d4a6d89a2a1a
root <root@localhost.localdomain>
2020-01-09 19:03:58 -0500

    Working disable unnecessary mods script

--- DisableUnnecessaryModules.sh
+++ DisableUnnecessaryModules.sh
@@ -6,50 +6,60 @@
 #source EnableModslist.sh
 
 #Returns an array of enabled module
-function GenerateEnableModsList() {
+GenerateEnableModsList() {
     ModuleOutput=$(apache2ctl -M)
     modsOnly=$(echo $ModuleOutput | cut -d ":"  -f 2)
-    #ModulesList=() #Array of enabled modules
-    IFS=')' 
-    read -ra ModulesList <<< $modsOnly 
-    #return $ModulesList
+    IFS=')' read -ra ModulesList <<< $modsOnly 
 }
 
+#loop through list, executing disabling module
+DisableMods(){
+  for i in ${Mods2Disable[@]}; do
+  echo "Disabling $i ${ModulesList[ $( expr  $i - 1 ) ]::-8}"
 
-Intro(){
-  echo "Best security practice suggest that Unnecessary or Unused modules be disabled.
+  #Disable module
+  a2dismod -q ${ModulesList[ $( expr $i - 1 ) ]::-15}
+  service apache2 restart
+  echo "Done."
+done
 }
 
-Here is a list of modules currently enabled on your web server:"
+echo "Best security practice suggest that Unnecessary or Unused modules be disabled."
+echo "Here is a list of modules currently enabled on your web server:"
 
 #Display list of enabled modules
 GenerateEnableModsList
-#$EnabledMods="$(GenerateEnableModsList)"
-#echo ${ModulesList[@]}
 
-
-for ((i = 0 ; i < ${#ModulesList[@]} ; i++)); do
+#Display list enabled modules on the screen
+for ((i = 0 ; i < ${#ModulesList[@]} ; i++ )); do
   echo $( expr $i + 1 )  ${ModulesList[i]::-8} 
+#  echo "i is $i"
+#  echo -e "$( expr $i + 1 )  ${ModulesList[i]::-8} \t $( expr $i + 2 )  ${ModulesList[ $( expr $i + 1 ) ]::-8} \t $( expr $i + 3 )  ${ModulesList[ $( expr $i + 2 ) ]::-8} "
 done
 
 echo "Which modules would you like to disable? Enter Corresponding numbers separated by spaces( ) i.e. 3 11 15 23"
 
 #Read in selection into list
-IFS=' ' read -ra Mods2Enable
-#echo ${Mods2Enable[1]}
+IFS=' ' read -ra Mods2Disable
 
 #Confirm selection
-echo "You want to disable the following modules? Enter Y to confirm or N to reenter your selection"
-for ((i = 0 ; i < ${#Mods2Enable[@]} ; i++)); do
-  echo "${ModulesList[ $( expr  ${Mods2Enable[i]} - 1) ]::-8}"
+echo "You want to disable the following modules? Enter y to confirm or n to reenter your selection"
+for ((i = 0 ; i < ${#Mods2Disable[@]} ; i++)); do
+  echo "${Mods2Disable[i]} ${ModulesList[ $( expr  ${Mods2Disable[i]} - 1) ]::-8}"
 done
 
+IFS=' ' read -ra answer
 
+if [ ${answer}=='y' ]; then
+  DisableMods  
+elif [ ${answer}=='n' ]; then
+  echo "Here is a list of modules currently enabled on your web server:"
+  for ((i = 0 ; i < ${#ModulesList[@]} ; i++)); do
+    echo $( expr $i + 1 )  ${ModulesList[i]::-8} 
+  done
 
-#loop through list, executing disabling module
-for ((i = 0 ; i < ${#Mods2Enable[@]} ; i++)); do
-  echo "Disabling ${ModulesList[ $( expr  ${Mods2Enable[i]} - 1) ]::-8}"
+  echo "Which modules would you like to disable? Enter Corresponding numbers separated by spaces, i.e. 3 11 15 23"
 
-  #Disable command
-  # a2dismod <Module>
-done
+  #Read in selection into list
+  IFS=' ' read -ra Mods2Disable
+fi;
